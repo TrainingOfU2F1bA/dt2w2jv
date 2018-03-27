7a2cb558e50ec87d57464c41d704948fcd3f6bd8
saul <eternx@163.com>
2018-03-27 16:06:43 +0800

    <009> refactor[PraticeC]:pass the PracticeCTest of section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,32 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
-    Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
+    Map<String, Integer> createUpdatedCollection(List<String> collectionA, Map<String, List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> collection3 = countSameElements(collectionA);
+        return createUpdatedCollection(collection3,object);
+    }
 
-        return null;
+    Map<String, Integer> countSameElements(List<String> collection1) {
+        HashMap<String, Integer> map = new HashMap<>();
+        collection1.stream().forEach(x -> {
+            if (!map.containsKey(x)) map.put(x, 1);
+            else map.put(x, map.get(x) + 1);
+        });
+        return map;
+    }
+
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
+        object.values().stream().flatMap(x -> x.stream()).forEach(x -> {
+            if (collectionA.containsKey(x)) {
+                Integer integer = collectionA.get(x);
+                collectionA.put(x, integer < 3 ? integer : integer - integer / 3);
+            }
+        });
+        return collectionA;
     }
 }
