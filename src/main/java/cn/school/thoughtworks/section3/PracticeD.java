f1782931436371495e80d53699ce98609e94b63c
saul <eternx@163.com>
2018-03-27 16:11:02 +0800

    <010> refactor[PraticeD]:pass the PracticeDTest of section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,53 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class PracticeD {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> collection3 = countSameElements(collectionA);
+        return createUpdatedCollection(collection3,object);
+    }
 
-        return null;
+    Map<String, Integer> countSameElements(List<String> collection1) {
+        HashMap<String, Integer> map = new HashMap<>();
+        collection1.stream().map(mapToEntry()).forEach(countThenPut(map));
+        return map;
+    }
+
+    private Function<String, Map.Entry<String, Integer>> mapToEntry() {
+        return x -> {
+            int i = x.lastIndexOf('-');
+            return new AbstractMap.SimpleEntry<String, Integer>(
+                    x.substring(0,i!=-1?i:x.length()),
+                    Integer.valueOf(i!=-1?x.substring(i+1):"1")
+            );
+        };
+    }
+
+    private Consumer<Map.Entry<String, Integer>> countThenPut(HashMap<String, Integer> map) {
+        return x -> {
+            String key = x.getKey();
+            if (!map.containsKey(key)) {
+                map.put(key,x.getValue());
+            }else {
+                map.put(key,map.get(key)+x.getValue());
+            }
+        };
+    }
+
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
+        object.values().stream().flatMap(x -> x.stream()).forEach(x -> {
+            if (collectionA.containsKey(x)) {
+                Integer integer = collectionA.get(x);
+                collectionA.put(x, integer < 3 ? integer : integer - integer / 3);
+            }
+        });
+        return collectionA;
     }
 }
