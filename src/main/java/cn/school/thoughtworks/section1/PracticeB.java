92f811aa9282717b4b3366a84610195cec5cd7c4
saul <eternx@163.com>
2018-03-27 10:49:56 +0800

    <002> refactor[PraticeB]:pass the PracticeBTest of section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,13 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        HashSet<String> set = new HashSet<>(collection2.stream().flatMap(x -> x.stream()).collect(Collectors.toList()));
+        return collection1.stream().filter(x->set.contains(x)).collect(Collectors.toList());
     }
 }
