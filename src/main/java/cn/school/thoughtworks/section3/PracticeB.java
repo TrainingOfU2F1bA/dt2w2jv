0f27b1941d428a0ff89ed0e6f25609b610734e83
saul <eternx@163.com>
2018-03-27 16:02:17 +0800

    <008> refactor[PraticeB]:pass the PracticeBTest of section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+    Map<String,Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
+        object.values().stream().flatMap(x -> x.stream()).forEach(x -> {
+            if (collectionA.containsKey(x)) {
+                Integer integer = collectionA.get(x);
+                collectionA.put(x, integer<3?integer:integer-integer/3);
+            }
+        });
+        return collectionA;
     }
 }
