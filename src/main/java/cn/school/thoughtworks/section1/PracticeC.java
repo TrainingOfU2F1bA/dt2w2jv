7c423647d961e96ed4529ab64f7c619d3b5023e0
saul <eternx@163.com>
2018-03-27 11:12:39 +0800

    <003> refactor[PraticeC]:pass the PracticeCTest of section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,14 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        HashSet<String> set= new HashSet<>(collection2.values().stream().flatMap(x -> x.stream()).collect(Collectors.toList()));
+        return collection1.stream().filter(x->set.contains(x)).collect(Collectors.toList());
     }
 }
